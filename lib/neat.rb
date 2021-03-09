14a917abb0c820e3822eb6a78edc503f462e09a1
Thiago Belem <github@thiagobelem.net>
2021-03-09 00:26:32 -0300

    Initial commit

---
+++ lib/neat.rb
@@ -0,0 +1,6 @@
+require "neat/version"
+
+module Neat
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/neat_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Neat do
+  it "has a version number" do
+    expect(Neat::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
