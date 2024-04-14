5ad909adae35730b2c4d95ef99d32aada6e40a39
Thiago Belem <github@thiagobelem.net>
2024-04-14 14:31:10 -0300

    Initial commit

---
+++ lib/neat.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'neat/version'
+
+module Neat
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/neat_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Neat do
+  it 'has a version number' do
+    expect(Neat::VERSION).not_to be nil
+  end
+end
